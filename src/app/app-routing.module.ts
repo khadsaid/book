2d20970ee4dfa213ef14752a69309cd4d727f117
unknown <khadijasuleiman7027@gmail.com>
2023-06-19 06:40:06 -0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MainlayoutComponent } from './layouts/mainlayout/mainlayout.component';
+import { LoginComponent } from './login/login.component';
+import { DashComponent } from './dash/dash.component';
+import { BookingComponent } from './booking/booking.component';
+import { AdminComponent } from './admin/admin.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'login',component:LoginComponent},
+  {path:'dash',component:DashComponent},
+ 
+    {path:'booking',component:BookingComponent},
+  
+  {path:'mainlayout',component:MainlayoutComponent},
+  {path:'admin', component:AdminComponent}
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
